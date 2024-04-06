2b53e3e4c39f4d461a8bf6e6697edc1ba919ee7b
Sy <111945571+SuXiFisherman@users.noreply.github.com>
2024-04-06 13:41:11 +0800

    Delete SyFolder/homepage.js

--- SyFolder/homepage.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll(".navbar li");
-console.log(items);
-items.forEach((item) => {
-    item.addEventListener("click", () =>{
-        document.querySelector("li.active").classList.remove("active");
-        item.classList.add("active");
-    });
-});
